45b45069cc76ca8f770993a3145fdb7c422247dd
Juan <juanescudero328@gmail.com>
2025-03-19 11:38:00 -0300

    segunda modificacion

--- questions.py
+++ questions.py
@@ -13,14 +13,17 @@ questions = [
 # Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
 answers = [
     ("size()", "len()", "length()", "count()"),
-    ("3.14", "42", "10", "True"),
+    ("3.14", "'42'", "10", "True"),
     ("input()", "scan()", "read()", "ask()"),
-    ("// Esto es un comentario", "/* Esto es un comentario */", "# Esto es un comentario", "-- Esto es un comentario"),
+    ("// Esto es un comentario", "/* Esto es un comentario */", "-- Esto es un comentario", "# Esto es un comentario"),
     ("=", "==", "!=", "==="),
 ]
 
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+
+# Inicializo puntaje
+puntaje = float(0) 
 
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
@@ -46,11 +49,18 @@ for _ in range(3):
     # Se verifica si la respuesta es correcta 
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntaje += 1
             break
         else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
-            print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index][correct_answers_index[question_index]])
+            if intento == 0:
+                print("Incorrecto")
+            else:
+                print("Incorrecto. La respuesta correcta es:")
+                print(answers[question_index][correct_answers_index[question_index]])
+            puntaje -= 0.5
     # Se imprime un blanco al final de la pregunta
     print()
+print("Tu puntaje es")
+print(puntaje)
